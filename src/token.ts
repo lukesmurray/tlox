b611ec3833ca22951489099968f6e3d8b495cc74
Luke Murray <lukepigeonmail@gmail.com>
2022-09-18 17:19:02 -0400

    add token

---
+++ src/token.ts
@@ -0,0 +1,19 @@
+import { TokenType } from "./tokenType";
+
+class Token {
+  public type: TokenType;
+  public lexeme: string;
+  public literal: any;
+  public line: number;
+
+  constructor(type: TokenType, lexeme: string, literal: any, line: number) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  public toString(): string {
+    return `${this.type} ${this.lexeme} ${this.literal}`;
+  }
+}
